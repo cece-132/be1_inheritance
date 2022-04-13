4d4871af203f257a333be822740e00726b478027
Chris Simmons <chris@turing.edu>
2022-04-13 11:55:37 -0400

    feat: completed intern

--- lesson_example/lib/employee.rb
+++ lesson_example/lib/employee.rb
@@ -1,9 +1,13 @@
+require 'pry'
+
 class Employee
   attr_reader :name, :id
 
   def initialize(name, id)
     @name = name
     @id = id
+    # If we uncomment this pry, what will be output when we type `self` in the Terminal?
+    #  binding.pry
   end
 
   def total_compensation
--- lesson_example/lib/intern.rb
+++ lesson_example/lib/intern.rb
@@ -1,8 +1,11 @@
-class Intern
+require 'employee'
+class Intern < Employee
   attr_reader :base_salary
-  
-  def initialize(base_salary)
+
+  def initialize(base_salary, name, id)
     @base_salary = base_salary
+    @bonus = 0 # "nil can't be coerced into Integer". So, we need to define a basic @bonus so that Employee's total_compensation will work. 
+    super(name, id)
   end
 
   def get_coffee
@@ -10,7 +13,8 @@ class Intern
   end
 
   def benefits
-    [:gets_to_get_me_coffee]
+    # [:gets_to_get_me_coffee]
+    super.unshift(:gets_to_get_me_coffee) # can also use super.push or <<
   end
 
 end
