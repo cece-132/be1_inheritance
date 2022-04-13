2e9c7a55403139d8a02d3895c3a70b88badd8fa0
Chris Simmons <chris@turing.edu>
2022-04-13 12:02:23 -0400

    feat: completed manager

--- lesson_example/lib/employee.rb
+++ lesson_example/lib/employee.rb
@@ -6,11 +6,13 @@ class Employee
   def initialize(name, id)
     @name = name
     @id = id
-    # If we uncomment this pry, what will be output when we type `self` in the Terminal?
+    # If we uncomment this pry, what will be output when we type `self` or `self.class` in the Terminal?
     #  binding.pry
   end
 
   def total_compensation
+    # Uncomment the pry below, and check what the value of @base_salary and @bonus is. Where do these values come from?
+    # binding.pry
     @base_salary + @bonus
   end
 
